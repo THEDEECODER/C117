6734d18d7b4a0693ee0c6c47b65d2f0754741942
Anmol Aryan <87566476+THEDEECODER@users.noreply.github.com>
2022-03-05 19:19:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 function setup(){
     canvas = createCanvas(200, 200);
     canvas.center();
-    background("red");
+    background("white");
+    canvas.mouseReleased(classifyCanvas)
+    synth = window.speechSynthesis;
 }
 function preload(){
-
+classifiy = ml5.imageClassifier('DoodleNet');
 }
 function draw(){
-
+stroke(0);
+strokeWeight(13);
+if(mouseIsPressed){
+line(pmouseX, pmouseY, mouseX, mouseY);}
 }
 function ClearCanvas(){
-    background("red");
+    background("white");
+}
+function classifyCanvas(){
+    classifiy.classify(canvas, gotResults)
+}
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{console.log(results);
+    document.getElementById("lbl1").innerHTML = "Label : " + results[0].label;
+    document.getElementById("lbl2").innerHTML = "Confidence : " + Math.round(results[0].confidence * 100) + "%";
+    speak_data = "the sketch is " + results[0].label;
+    utterthis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+}
 }
